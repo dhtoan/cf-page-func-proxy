05a5ec2ef6867c1e064b4dec9bf45df2c015cde7
David Do <do@doligo.net>
2024-07-20 17:58:39 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="img.aunomay.com";
+        url.hostname="aunomay.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
