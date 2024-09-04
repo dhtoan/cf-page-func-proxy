e990d55b11d3763e5c31961154dbaff599e5652d
David Do <do@doligo.net>
2024-09-05 02:57:22 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="i0.wp.com";
+        url.hostname="aunomay.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
